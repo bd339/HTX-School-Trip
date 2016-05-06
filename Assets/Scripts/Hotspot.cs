eaad7fa03de8363f0fa1a9e61a8dab4130d58291
bd339 <bd339@tuta.io>
2016-05-06 22:36:10 +0200

    Finalize support for "non-NPC" hotspots

--- Assets/Scripts/Hotspot.cs
+++ Assets/Scripts/Hotspot.cs
@@ -20,7 +20,17 @@ class Hotspot : MonoBehaviour {
     }
 
     protected void OnMouseDown () {
-        // remove help
+        foreach (var h in FindObjectsOfType<Hotspot> ()) {
+            h.GetComponent<BoxCollider> ().enabled = false;
+        }
+
+        CooperScript.Engine.stalled = true;
+        CooperScript.Engine.AddSecondaryScript (this);
+        CooperScript.Engine.stalled = false;
+
+        InvestigationControls.Controls.LeaveInvestigationMode ();
+        Dialogue.ChatboxDialogue.LeaveDialogueMode ();
+        // leave court mode
     }
 
     void OnMouseEnter () {
--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -5,23 +5,11 @@ class NPC : Hotspot {
     new protected void OnMouseDown () {
         Camera.main.GetComponent<InvestigationControls> ().Target = transform.position;
 
-        foreach (var h in FindObjectsOfType<Hotspot> ()) {
-            h.GetComponent<BoxCollider> ().enabled = false;
-        }
-
-        CooperScript.Engine.stalled = true;
-        CooperScript.Engine.AddSecondaryScript (this);
-
         StartCoroutine (RunScript ());
     }
 
     private System.Collections.IEnumerator RunScript () {
         yield return new WaitWhile (() => Camera.main.GetComponent<InvestigationControls> ().HasTarget);
-        CooperScript.Engine.stalled = false;
-
-        InvestigationControls.Controls.LeaveInvestigationMode ();
-        Dialogue.ChatboxDialogue.LeaveDialogueMode ();
-        // leave court mode
 
         base.OnMouseDown ();
     }
